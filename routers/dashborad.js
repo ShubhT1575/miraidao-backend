082f761ad40f7ee388af2b7ccc127a45af44c50d
shubham <shubhamnadcab@gmail.com>
2025-02-26 18:13:18 +0530

    sh

--- routers/dashborad.js
+++ routers/dashborad.js
@@ -202,7 +202,7 @@ router.get("/newuserplacePool", async (req, res) => {
 router.get("/referralhistory", async (req, res) => {
   try {
     const { referrer } = req.query;
-    const data = await newuserplace.find({ referrer: referrer }).sort({ createdAt: -1 });
+    const data = await registration.find({ referrer: referrer }).sort({ createdAt: -1 });
 
     const mergedData = await Promise.all(data.map(async (record) => {
       const userDetails = await registration.findOne({ user: record.user }); // Find one user
